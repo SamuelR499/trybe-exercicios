3116f65f625e2f67ca1d946a10fd47b963c9ea2b
SamuelR499 <samuelribeiro2@hotmail.com>
2022-03-08 21:54:16 -0300

    resolução do exercicio 5 e 6

---
+++ fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-progamacao/dia-2-javaScript-array-e-loop-for/exercicio-6.js
@@ -0,0 +1,20 @@
+let numbers  = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let totalDeImpares = 0;
+let numImpar = 0;
+
+for (let i = 0; i <numbers.length; i += 1) {
+    if (numbers[i] % 2 != 0) {
+        numImpar = numbers[i];
+        console.log(numImpar, " bora!! mais um impar");
+        totalDeImpares += 1;
+    }else{
+        console.log(numbers[i], " ih!! esse é par");
+    }
+}
+    console.log("O total de impares é: ", totalDeImpares);
+
+if (totalDeImpares == 0) {
+    console.log("Nenhum numero impar encontrado!!");
+}
+
