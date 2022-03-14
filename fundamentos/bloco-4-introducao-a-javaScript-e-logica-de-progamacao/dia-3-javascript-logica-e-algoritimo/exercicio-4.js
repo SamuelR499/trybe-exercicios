bafb5dbebcf412599b5ce527a8682b973196672d
Samuelr499 <samuelribeiro2@hotmail.com>
2022-03-14 19:35:44 -0300

    resolve exercio 1 2 3 4

--- fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-progamacao/dia-3-javascript-logica-e-algoritimo/exercicio-4.js
+++ fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-progamacao/dia-3-javascript-logica-e-algoritimo/exercicio-4.js
@@ -1,6 +1,26 @@
-let numueroPrimo = 0;
-for (let i = 0; i <=50; i++){
-    if (numueroPrimo) {
-        
-    }
+//Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+
+//oque é im numero primo:
+// divididio somente por 1 e por ele mesmo:
+// 
+// i % i == 0 && i % z != 0
+
+let allNumbers = [];
+for(i = 0; i <= 50; i ++){
+    allNumbers.push(i);
+};
+// console.log(allNumbers);
+
+
+for(cont = 1; cont < allNumbers.length; cont ++){
+    if(allNumbers[i] % allNumbers[i] == 0 && allNumbers[i] % 
 }
+// if( 6 % 6 == 0 && 6 % 2 != 0 && 6 % 1 == 0){
+//     console.log(true);
+// }else{
+//     console.log(false);
+    
+// }
+
+//ser if( i % i == 0 && i % z != 0 && i % 1 == 0)
+// /terminar <-
