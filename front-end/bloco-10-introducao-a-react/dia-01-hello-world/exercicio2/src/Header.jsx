67b2378fa380f7b6b3e64057a52e9fbd01b9cd62
Samuelr499 <samuelribeiro2@hotmail.com>
2022-05-06 15:02:38 -0300

    Exercicos 10 dia 1

---
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world/exercicio2/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+class Header extends React.Component {
+  render() {
+    return(
+        <h1 className="frontend">Conteúdos de Front-End</h1>
+    )
+  }
+}
+
+export default Header;
