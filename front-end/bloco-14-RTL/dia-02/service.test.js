4f204d92d9f32438ea6254f1d3ac80c640518446
Samuelr499 <samuelribeiro2@hotmail.com>
2022-06-10 12:42:34 -0300

    Exercicio 15.1 lol

---
+++ front-end/bloco-14-RTL/dia-02/service.test.js
@@ -0,0 +1,78 @@
+const { test } = require('@jest/globals');
+const service = require('./service');
+
+describe("testando implementações", () => {
+  test("testando se a função foi chamada, qual seu retorno e quantas vezes foi chamada", () => {
+    service.randomNumber = jest.fn().mockReturnValue(10);
+
+    expect(service.randomNumber()).toBe(10);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  });
+
+  test("deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez", () => {
+    service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b)
+
+    expect(service.randomNumber(10, 2)).toBe(5);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(10, 2);
+  });
+  test("deve receber dois parâmetros e retornar a multipicação do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez", () => {
+    service.randomNumber = jest.fn().mockImplementationOnce((a, b, c) => a * b * c)
+
+    expect(service.randomNumber(10, 2, 4)).toBe(80);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(10, 2, 4);
+  });
+
+  test("mockando função que recebe um parâmetro e retorna seu dobro", () => {
+    service.randomNumber.mockReset();
+
+    expect(service.randomNumber).toHaveBeenCalledTimes(0);
+    service.randomNumber.mockImplementation((a) => a * 2);
+
+    expect(service.randomNumber(5)).toBe(10);
+    expect(service.randomNumber(3)).toBe(6);
+    expect(service.randomNumber(4)).not.toBe(9);
+
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(3);
+
+    expect(service.randomNumber).toHaveBeenCalledWith(5);
+    expect(service.randomNumber).toHaveBeenCalledWith(3);
+    expect(service.randomNumber).toHaveBeenCalledWith(4);
+
+    expect(service.randomNumber).toHaveBeenCalledWith(5);
+  });
+
+  test("mockando função para receber um parâmetro e retornar em caixa baixa", () => {
+    const mockFirstFunction = jest.spyOn(service, "firstFunction").mockImplementation(a => a.toLowerCase());
+
+    expect(mockFirstFunction("UPPERCASE")).toBe("uppercase");
+    expect(service.firstFunction).toHaveBeenCalledTimes(1);
+    expect(service.firstFunction).toHaveBeenCalledWith("UPPERCASE");
+  });
+
+  test("mockando função que recebe um parâmetro e retorna a última letra", () => {
+    const mockSecondFunction = jest.spyOn(service, "secondFunction").mockImplementation(a => a.charAt(a.length - 1));
+
+    expect(mockSecondFunction("letter")).toBe("r");
+    expect(service.secondFunction).toHaveBeenCalledTimes(1);
+    expect(service.secondFunction).toHaveBeenCalledWith("letter");
+  });
+
+  test("mockando função que recebe 3 parâmetros e os concatena", () => {
+    const mockThirdFunction = jest.spyOn(service, "thirdFunction").mockImplementation((a, b, c) => a.concat(b, c));
+
+    expect(mockThirdFunction("tr", "y", "be")).toBe("trybe");
+    expect(service.thirdFunction).toHaveBeenCalledTimes(1);
+    expect(service.thirdFunction).toHaveBeenCalledWith("tr", "y", "be")
+  });
+  test("restaurando a primeira função e verifica se ela retorna em caixa alta", () => {
+    service.firstFunction.mockRestore();
+
+    expect(service.firstFunction("lowercase")).toBe("LOWERCASE");
+  })
+})
