cb0a9523af442ccf1cbc69f20ecd704ed129448e
Samuelr499 <samuelribeiro2@hotmail.com>
2022-10-31 18:54:27 -0300

    feat: exercicios

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/exercicios/exercicio08.js
@@ -0,0 +1,15 @@
+// Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado.
+const numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
+}
+
+// console.log(numbers);
+
+/* Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um
+dos elementos por 2.
+*/
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index] / 2);
+};
