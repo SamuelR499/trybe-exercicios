f46e9644cd0924d8e9da0ebb16778b26ec23f10f
Samuelr499 <samuelribeiro2@hotmail.com>
2022-10-31 18:53:38 -0300

    feat: arrays

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-array-e-loop-for/arrays.js
@@ -0,0 +1,18 @@
+// Obtenha o valor “Serviços” do array menu:
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// Procure o índice do valor “Portfólio” do array menu:
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log('o índice do valor "Portfólio" é: ', indexOfPortfolio);
+
+// Adicione o valor “Contato” no final do array menu:
+// De olho na dica: Use o método .push()
+
+menu.push('contato');
+
+console.log(menu);
