7a9ea1ac6158f726d76f2705638984bbb6648b4f
Samuelr499 <samuelribeiro2@hotmail.com>
2022-10-31 18:11:33 -0300

    variaveis

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-primeiros-passos/variaveis.js
@@ -0,0 +1,23 @@
+// Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+const myName = "Samuel"
+
+// Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+const birthCity = "Tarumã";
+
+
+// Crie uma variável let chamada birthYear e atribua a ela o ano em que você nasceu.
+let birthYear = 1997;
+
+// Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+// Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+birthYear = 2023;
+console.log(birthYear);
+
+// Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro?
+// birthCity = "The Sky";
+// console.log(birthCity);
+// TypeError: Assignment to constant variable.
