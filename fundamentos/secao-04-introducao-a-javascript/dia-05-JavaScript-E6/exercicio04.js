975cd5dcdba8067516591d9a0b4a3264e293a13d
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-22 11:35:20 -0300

    feat: exericicio04

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-JavaScript-E6/exercicio04.js
@@ -0,0 +1,8 @@
+// Modifique a variável para que não ocorra Erro;
+// Altere a variável do tipo const para o tipo let.
+
+// const favoriteFood = 'Lasanha';
+
+let favoriteFood = 'Lasanha';
+favoriteFood = 'Hamburguer';
+console.log(favoriteFood);
