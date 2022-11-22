30ce23a6f59ec253ab56987bcc2be1f226c35cdb
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-22 11:35:15 -0300

    feat: exericicio03

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-JavaScript-E6/exercicio03.js
@@ -0,0 +1,10 @@
+/*
+Transforme a função numeroAleatorio em uma arrow function;
+*/
+
+// function numeroAleatorio() {
+//   return Math.random()
+// }
+const numeroAleatorio = () => Math.random()
+
+console.log(numeroAleatorio());
