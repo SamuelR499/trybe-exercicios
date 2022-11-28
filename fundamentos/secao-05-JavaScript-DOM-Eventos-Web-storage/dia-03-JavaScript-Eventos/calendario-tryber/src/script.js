61322462d819b0e8e3b1e67f882af630c49b41da
Samuelr499 <samuelribeiro2@hotmail.com>
2022-11-28 17:37:57 -0300

    feat: displayFridays

--- fundamentos/secao-05-JavaScript-DOM-Eventos-Web-storage/dia-03-JavaScript-Eventos/calendario-tryber/src/script.js
+++ fundamentos/secao-05-JavaScript-DOM-Eventos-Web-storage/dia-03-JavaScript-Eventos/calendario-tryber/src/script.js
@@ -100,3 +100,26 @@ const createFridayButton = (buttonName) => {
 }
 
 createFridayButton('Sexta-feira');
+
+// -----------------
+
+const displayFridays = (fridaysArray) => {
+  let getFridayButton = document.querySelector('#btn-friday');
+  let fridays = document.getElementsByClassName('friday');
+  let newFridayText = 'SEXTOU o/';
+
+  getFridayButton.addEventListener('click', () => {
+  for (let index = 0; index < fridays.length; index += 1) {
+    if (fridays[index].innerHTML !== newFridayText) {
+        fridays[index].innerHTML = newFridayText;
+    } else {
+        fridays[index].innerHTML = fridaysArray[index];
+      }
+    }
+  });
+}
+
+let decemberFridays = [ 4, 11, 18, 25 ];
+displayFridays(decemberFridays);
+
+// -----------------
